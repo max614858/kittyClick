b44490e8e4e9d2cc582aacd11d2788369a5fc733
max614858 <149180902+max614858@users.noreply.github.com>
2024-04-02 19:25:05 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,79 @@
-let kittyClicks = 0;
-let kittyMutliplier = 1
+document.addEventListener('DOMContentLoaded', function() {
+  let clickValue = 1;
+  let mutliplier = 1;
+  let soupKitty = document.getElementById("clickCat");
+  let soupAmount = document.getElementById('amount');
+  let displayKittos = document.getElementById('displayKittos')
+  let shopLink = document.getElementById('shopLink');
 
-kittyClicks = Number(localStorage.getItem('kittyClicks'))
-document.getElementById('amount').innerHTML = "kittyClicks: " + kittyClicks
+  if (clickValue) {
+    clickValue = Number(localStorage.getItem('clickValue'))
+  } else {
+    localStorage.setItem('clickValue', 1)
+  }
 
-document.getElementById("clickCat").addEventListener('click', function(event) {
-  kittyClicks += 1 * kittyMutliplier
-  console.log(kittyClicks)
-  localStorage.setItem('kittyClicks', kittyClicks);
-  document.getElementById('amount').innerHTML = "kittyClicks: " + kittyClicks  
-})
+  if (soupKitty) {
+    soupKitty.addEventListener('click', addClick)
+  }
 
-document.getElementById('shopLink').addEventListener('click', function(event) {
-  document.getElementById('displayKittos').innerHTML = "kittyClicks: " + kittyClicks  
+  if (localStorage.getItem('clicks')) {
+    kittyClicks = Number(localStorage.getItem('clicks'))
+  } else kittyClicks = localStorage.setItem('clicks', 0)
+
+
+  function addClick() {
+    localStorage.getItem('clicks')
+    kittyClicks += clickValue * mutliplier
+    soupAmount.innerHTML = `KittyClicks: ${kittyClicks}`;
+    localStorage.setItem('clicks', kittyClicks);
+  }
+
+
+  console.log('loaded')
+  if (soupAmount) {
+    soupAmount.innerHTML = `KittyClicks: ${kittyClicks}`;
+  }
+  if (displayKittos) {
+    let vA = localStorage.getItem('vA');
+    displayKittos.innerHTML = `KittyClicks: ${kittyClicks}`;
+    let buttonList = document.querySelectorAll(".shopButtons")
+    buttonList[0].innerHTML = "<h4>jimpy clicker<br> $" + `${vA}` + "kitto</h4>"
+    function getPrice() {
+      let buttonOrder = []
+      for (let button of buttonList) {
+        if (button.innerHTML.indexOf('$') != -1) {
+          buttonOrder.push(button)
+          button.addEventListener('click', function(event) {
+            let price = button.innerHTML.slice(button.innerHTML.indexOf('$') + 1, button.innerHTML.lastIndexOf('k'));
+            let pay = Number(price)
+            if (pay > kittyClicks) {
+
+            }
+            else {
+              (kittyClicks -= pay)
+              displayKittos.innerHTML = `KittyClicks: ${kittyClicks}`;
+              localStorage.setItem('clicks', kittyClicks);
+              if (buttonOrder[0]) {
+                clickValue ++
+                localStorage.setItem('clickValue', clickValue)
+                pay = 100 * clickValue * mutliplier
+                localStorage.setItem('vA', pay)
+                button.innerHTML = "<h4>jimpy clicker<br> $" + `${pay}` + "kitto</h4>"
+              }
+              if (buttonOrder[1]) {
+
+                localStorage.setItem('clickValue', clickValue)
+
+              }
+                
+              if (buttonOrder[2]) {
+                localStorage.setItem('clickValue', clickValue)
+
+              }
+          }
+          })
+        }
+        }}
+        getPrice()
+  }
 })
